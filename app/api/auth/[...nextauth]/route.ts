8ec79508570f91ee48e6c6c9fb7e47d291c65f90
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-05 19:40:29 +0100

    dploy

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import {PrismaAdapter} from '@auth/prisma-adapter'
 import prisma from "@/lib/prismadb"
 
+
 export const authOptions : AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers:[
