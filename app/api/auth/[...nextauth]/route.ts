0285f674f6c13381e95f342f933c219c0b7740d9
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-10-07 18:18:11 +0100

    dploy

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth"
-import GitHubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
-import {PrismaAdapter} from '@auth/prisma-adapter'
-import prisma from "@/lib/prismadb"
-
-export const authOptions : AuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers:[
-    GitHubProvider({
-        clientId: process.env.CLIENT_ID_GITHUB as string,
-        clientSecret: process.env.CLIENT_SECRET_GITHUB as string
-    }),
-    GoogleProvider({
-        clientId: process.env.CLIENT_ID_GOOGLE as string,
-        clientSecret: process.env.CLIENT_SECRET_GOOGLE as string,
-    })
-],
-pages: {
-    signIn:"/signin",
-},
-secret: process.env.NEXTAUTH_SECRET as string
-}
+import NextAuth from "next-auth"
+import { authOptions } from "@/lib/authOptions"
 
 const handler = NextAuth(authOptions)
 
